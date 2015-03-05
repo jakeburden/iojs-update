f4b4c180a481b6530a31667420e20d92d90fbf8e
Jacob Burden <Jacob.JW.Burden@gmail.com>
2015-03-05 17:55:01 -0500

    added nightly updater

---
+++ iojs-update-nightly.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+echo "> Getting latest version number"
+
+VERSION=v${1:-$(curl https://iojs.org/download/nightly/index.json | sed -e 's/^.*"version":"\([^"]*\)".*$/\1/' | head -n 2 | tail -n -1 | cut -c 2-)}
+IOJS=iojs-${VERSION}-linux-x64
+
+echo "> Downloading $VERSION of iojs"
+curl -s https://iojs.org/download/nightly/${VERSION}/${IOJS}.tar.xz | tar xvfJ -
+
+echo "> Setting ownership of /usr/local to $USER"
+sudo chown -R ${USER}:${USER} /usr/local
+
+echo "> Moving extracted iojs binaries"
+rm -rf /usr/local/lib/${IOJS} && mv ${IOJS} /usr/local/lib
+
+echo "> Symlinks"
+sudo rm -f /usr/local/bin/{iojs,node,npm,node-gyp}
+ln -s /usr/local/lib/${IOJS}/bin/iojs /usr/local/bin/iojs
+ln -s /usr/local/lib/${IOJS}/bin/node /usr/local/bin/node
+ln -s /usr/local/lib/${IOJS}/bin/npm /usr/local/bin/npm
+
+echo "iojs version: $(iojs -v), npm version: $(npm -v)"
+
--- readme.markdown
+++ readme.markdown
@@ -1,11 +1,19 @@
 # Install the latest version of io.js
 
 
-## Usage
+## Usage from the terminal
 
+
+### Install the latest (stable) version of io.js
 
 ```
-$ chmod +x iojs-update.sh
-$ ./iojs-update.sh
+chmod +x iojs-update.sh
+./iojs-update.sh
 ```
 
+### Install the latest (nightly) version of io.js
+
+```
+chmod +x iojs-update-nightly.sh
+./iojs-update-nightly.sh
+```
